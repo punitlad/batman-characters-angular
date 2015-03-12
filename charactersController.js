6e9531570e56f20e73249a4a41de02787aa8c03a
Punit Lad <plad@thoughtworks.com>
2015-03-12 15:50:15 -0400

    added character specific page

--- charactersController.js
+++ charactersController.js
@@ -1,10 +1,45 @@
-var batmanCharacters = angular.module('batmanCharacters', []);
+var batmanCharacters = angular.module('batmanCharacters', ['ngRoute']);
 
-batmanCharacters.controller('charactersController', ['$scope', '$http', 
-    function($scope, $http) {
-
+batmanCharacters
+.config(['$routeProvider', 
+	function ($routeProvider) {
+		$routeProvider
+			.when('/character/:name',
+			{
+				templateUrl: "characterPage.html",
+				controller: "characterController"
+			})
+			.when('/',
+			{
+				template: "hello"
+				// templateUrl: "indexz.html",
+				// controller: "charactersController"
+			});
+		}])
+.run(['$anchorScroll', 
+	function($anchorScroll) {
+		$anchorScroll.yOffset = 50; 
+	}])	
+.controller('charactersController', ['$scope', '$http', '$location', '$anchorScroll',
+    function($scope, $http, $location, $anchorScroll) {
         $http.get('characters.json').success( function(data) {
             $scope.characters = data
         });
-    }
-]);
+
+        $scope.jumpToId = function(id) {
+        	$location.hash(id);
+        	$anchorScroll();
+        };
+    }])
+.controller('characterController', ['$scope', '$routeParams',
+	function($scope, $routeParams) {
+		$http.get('characters.json').success( function(data) {
+        	for(var i=0; i<data.length; i++) {
+        		if (data[i].characterName == $routeParams.name) {
+        			$scope.character = data[i];
+        			break;
+        		}
+        	}
+        });
+	}])
+;
